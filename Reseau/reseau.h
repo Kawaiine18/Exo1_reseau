e66fa2fdfc77fd165999b1f39db4dc6bca1eb71c
Kawaiine18 <167618690+Kawaiine18@users.noreply.github.com>
2024-09-19 10:44:36 +0300

    Update reseau.h

--- Reseau/reseau.h
+++ Reseau/reseau.h
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define d 255
+//Pour recupérer l'addresse ip entrez par l'utilisateur
 char* getdata(char* ip);
-void verification(char* ip);
+//Pour recupérer le Masque de sous reseau entrez par l'utilisateur
+char* getdata_2(char* netmask);
+ //Pour verifier si l'addresse ip et le Masque de sous reseau est valide ou non 
+void verification(char* ip,char* netmask);
+//liberer les tableaux
+void free_all_array(char* ip,char* netmask);
